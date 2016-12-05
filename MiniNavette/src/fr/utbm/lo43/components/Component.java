cc483bb27f1545811d42958ebed94da549794a58
Thomas Gredin <t.gredin@gmx.fr>
2016-12-05 15:02:56 +0100

    Ajout des méthodes dans les classes Entity et Component

--- MiniNavette/src/fr/utbm/lo43/components/Component.java
+++ MiniNavette/src/fr/utbm/lo43/components/Component.java
@@ -1,5 +1,16 @@
 package fr.utbm.lo43.components;
 
-public abstract class Component {
+public abstract class Component 
+{
 
+	public Component()
+	{
+		
+	}
+	
+	public abstract void init();
+	public abstract void update();
+	public abstract void render();
+	public abstract void cleanUp();
+	
 }
--- MiniNavette/src/fr/utbm/lo43/entities/Entity.java
+++ MiniNavette/src/fr/utbm/lo43/entities/Entity.java
@@ -1,5 +1,37 @@
 package fr.utbm.lo43.entities;
 
-public class Entity {
+import java.util.ArrayList;
 
+import fr.utbm.lo43.components.Component;
+
+public class Entity 
+{
+
+	private ArrayList<Component> components;
+	
+	public Entity()
+	{
+		
+	}
+	
+	public void init()
+	{
+		
+	}
+	
+	public void update()
+	{
+		
+	}
+	
+	public void render()
+	{
+		
+	}
+	
+	public void cleanUp()
+	{
+		
+	}
+	
 }
