2af6f6e7d1933c665adcc8935a0536afd54fe67e
Thomas Gredin <t.gredin@gmx.fr>
2016-12-05 15:21:35 +0100

    Ajout de Components

---
+++ MiniNavette/src/fr/utbm/lo43/components/ClickableComponent.java
@@ -0,0 +1,31 @@
+package fr.utbm.lo43.components;
+
+import fr.utbm.lo43.entities.Entity;
+
+public class ClickableComponent extends Component
+{
+	public ClickableComponent(Entity p) 
+	{
+		super(p);
+	}
+
+	@Override
+	public void init() 
+	{
+	}
+
+	@Override
+	public void update() 
+	{
+	}
+
+	@Override
+	public void render() 
+	{
+	}
+
+	@Override
+	public void cleanUp() 
+	{
+	}
+}
---
+++ MiniNavette/src/fr/utbm/lo43/components/SpriteComponent.java
@@ -0,0 +1,35 @@
+package fr.utbm.lo43.components;
+
+import org.newdawn.slick.Image;
+
+import fr.utbm.lo43.entities.Entity;
+
+public class SpriteComponent extends Component
+{
+	Image image;
+	
+	public SpriteComponent(Entity p) 
+	{
+		super(p);
+	}
+	
+	@Override
+	public void init() 
+	{
+	}
+
+	@Override
+	public void update() 
+	{
+	}
+
+	@Override
+	public void render() 
+	{
+	}
+
+	@Override
+	public void cleanUp() 
+	{
+	}
+}
