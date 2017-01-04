e130a74913897058d6f7f15936fa819ff177a01a
Thomas Gredin <t.gredin@gmx.fr>
2017-01-04 12:31:18 +0100

    Ajout de la classe EntityCollection

---
+++ MiniNavette/src/fr/utbm/lo43/entities/EntityCollection.java
@@ -0,0 +1,37 @@
+package fr.utbm.lo43.entities;
+
+import java.util.ArrayList;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class EntityCollection implements EntityUpdateable, EntityDrawable
+{
+	ArrayList<Entity> entities;
+	
+	public EntityCollection()
+	{
+		entities = new ArrayList<Entity>();
+	}
+	
+	public void add(Entity _entity)
+	{
+		
+	}
+	
+	public void delete(Entity _entity)
+	{
+		
+	}
+
+	@Override
+	public void render(Graphics arg2) 
+	{
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg) 
+	{
+	}
+}
