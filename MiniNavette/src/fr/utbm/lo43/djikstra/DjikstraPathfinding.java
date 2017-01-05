b5757de408018676440c7b678cd2f2b81cec9489
Nahil <nahil.zamiati@gmail.com>
2017-01-05 15:05:22 +0100

    Debut package Djikstra : Class Djikstra, Classe Node

---
+++ MiniNavette/src/fr/utbm/lo43/djikstra/DjikstraPathfinding.java
@@ -0,0 +1,30 @@
+package fr.utbm.lo43.djikstra;
+
+import java.util.ArrayList;
+
+public class DjikstraPathfinding<T> {
+
+	private ArrayList<Node<T>> nodesList;
+
+	public DjikstraPathfinding(ArrayList<T> ElementsList) {
+		super();
+		nodesList = new ArrayList<>();
+		for(T element : ElementsList ){
+			nodesList.add(new Node<T>(element));
+		}
+		
+		
+	}
+	
+	public ArrayList<T> getShortestPath(T start, T end){
+		for (Node<T> n : nodesList){
+			if (n.element == start){
+				
+			}
+		}
+		return null;
+		
+	}
+	
+
+}
