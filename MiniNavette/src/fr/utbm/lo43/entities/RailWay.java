b996bb1ad0e3d8aa48f3a1920533b7e855ac222c
Thomas Gredin <t.gredin@gmx.fr>
2017-01-07 10:41:44 +0100

    Fin de session de code matinale

--- MiniNavette/src/fr/utbm/lo43/entities/RailWay.java
+++ MiniNavette/src/fr/utbm/lo43/entities/RailWay.java
@@ -21,24 +21,30 @@ public class RailWay extends Entity implements EntityDrawable
 		plot.setClosed(false);
 		plotWidth = Map.GRID_SIZE;
 		
+		int startX = 0;
+		int startY = 0;
+		
 		/* Déterminer si on part d'un plan horizontal ou vertical */
 		Random random = new Random();
 		if(random.nextInt(1) == 0)
 		{
 			/* plan horizontal */
-			int startX = random.nextInt((Map.WIDTH - 200)) + 200;
-			int startY = 0;
+			startX = random.nextInt((Map.WIDTH - 200)) + 200;
+			startY = 0;
 			
 			plot.addPoint(startX, startY);
 		}
 		else
 		{
 			/* plan vertical */
-			int startX = 0;
-			int startY = random.nextInt((Map.HEIGHT - 200)) + 200;
+			startX = 0;
+			startY = random.nextInt((Map.HEIGHT - 200)) + 200;
 			
 			plot.addPoint(startX, startY);
 		}
+		
+		int actualX = startX;
+		int actualY = startY;
 	}
 
 	@Override
